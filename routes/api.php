c077d039358e6f2d7ec54ac5012118d4984d57a0
Antoine Voituron <avoitu@orange.fr>
2025-02-26 23:50:58 +0100

    [EDIT][WIP] Inscription of user from register but errorwith no return under JSON form

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/test', function () {
+    return response()->file(storage_path('test.php'));
+});
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    foreach ($user->tokens as $token) {
+        // TODO enlever avant la production
+        echo("token créé");
+        echo($token);
+    }
+ 
+    return ['token' => $token->plainTextToken];
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
